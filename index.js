4e1049b059c14464f8c5c2f82cb12e95056f46f5
David Corticchiato <david.corticchiato@gmail.com>
2019-05-06 19:06:18 -0400

    export handler

--- index.js
+++ index.js
@@ -47,6 +47,9 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
+/* server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
-});
+}); */
+
+module.exports = server.createHandler();
+
