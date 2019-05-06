ec694f4611f830954654264dbf1f0799cc1d3dd4
David Corticchiato <david.corticchiato@gmail.com>
2019-05-06 19:14:04 -0400

    use apollo-server

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server-micro');
+const { ApolloServer, gql } = require('apollo-server');
 
 const books = [
   { id: 1, title: 'The Trials of Brother Jero', rating: 8, authorId: 1 },
