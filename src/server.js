d13d1ace60e53dbc2793b988a02896c25cc2cbdb
David Corticchiato <david.corticchiato@gmail.com>
2019-05-12 14:09:19 -0400

    link an express app

--- src/server.js
+++ src/server.js
@@ -1,98 +1,106 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+// const { RESTDataSource } = require("apollo-datasource-rest");
 
-class PokeAPI extends RESTDataSource {
-  constructor() {
-    super();
-    this.baseURL = "https://pokeapi.co/api/v2";
-  }
+// class PokeAPI extends RESTDataSource {
+//   constructor() {
+//     super();
+//     this.baseURL = "https://pokeapi.co/api/v2";
+//   }
 
-  async getByName(name) {
-    return this.get(`/pokemon/${name}`);
-  }
+//   async getByName(name) {
+//     return this.get(`/pokemon/${name}`);
+//   }
 
-  async getList(offset) {
-    const data = await this.get(`/pokemon?offset=${offset}`);
-    const promises = data.results.map(pokemon => this.getByName(pokemon.name));
+//   async getList(offset) {
+//     const data = await this.get(`/pokemon?offset=${offset}`);
+//     const promises = data.results.map(pokemon => this.getByName(pokemon.name));
 
-    return Promise.all(promises);
-  }
-}
+//     return Promise.all(promises);
+//   }
+// }
 
-function capitalize(name) {
-  return name.charAt(0).toUpperCase() + name.slice(1);
-}
+// function capitalize(name) {
+//   return name.charAt(0).toUpperCase() + name.slice(1);
+// }
 
-function extractOptionalSprites(sprites, backName, frontName) {
-  const valid =
-    sprites.hasOwnProperty(backName) &&
-    sprites.hasOwnProperty(frontName) &&
-    typeof sprites[backName] === "string" &&
-    typeof sprites[frontName] === "string" &&
-    sprites[backName].length > 0 &&
-    sprites[frontName].length > 0;
+// function extractOptionalSprites(sprites, backName, frontName) {
+//   const valid =
+//     sprites.hasOwnProperty(backName) &&
+//     sprites.hasOwnProperty(frontName) &&
+//     typeof sprites[backName] === "string" &&
+//     typeof sprites[frontName] === "string" &&
+//     sprites[backName].length > 0 &&
+//     sprites[frontName].length > 0;
 
-  return valid
-    ? { back: sprites[backName], front: sprites[frontName] }
-    : null;
-}
+//   return valid
+//     ? { back: sprites[backName], front: sprites[frontName] }
+//     : null;
+// }
 
-function createServer({ ApolloServer, gql }) {
-  const typeDefs = gql`
-    type PokemonPostureSpriteSheet {
-      back: String!
-      front: String!
-    }
+// function createServer({ ApolloServer, gql }) {
+//   const typeDefs = gql`
+//     type PokemonPostureSpriteSheet {
+//       back: String!
+//       front: String!
+//     }
 
-    type PokemonSpriteSheetCollection {
-      default: PokemonPostureSpriteSheet!
-      female: PokemonPostureSpriteSheet
-      shiny: PokemonPostureSpriteSheet
-      shinyFemale: PokemonPostureSpriteSheet
-    }
+//     type PokemonSpriteSheetCollection {
+//       default: PokemonPostureSpriteSheet!
+//       female: PokemonPostureSpriteSheet
+//       shiny: PokemonPostureSpriteSheet
+//       shinyFemale: PokemonPostureSpriteSheet
+//     }
 
-    type Pokemon {
-      id: Int!
-      name: String!
-      sprites: PokemonSpriteSheetCollection!
-    }
+//     type Pokemon {
+//       id: Int!
+//       name: String!
+//       sprites: PokemonSpriteSheetCollection!
+//     }
 
-    type Query {
-      pokemons(offset: Int!): [Pokemon!]!
-    }
-  `;
+//     type Query {
+//       pokemons(offset: Int!): [Pokemon!]!
+//     }
+//   `;
 
-  const dataSources = () => {
-    return {
-      pokeAPI: new PokeAPI()
-    };
-  };
+//   const dataSources = () => {
+//     return {
+//       pokeAPI: new PokeAPI()
+//     };
+//   };
 
-  const resolvers = {
-    Query: {
-      pokemons: (_source, { offset }, { dataSources }) =>
-        dataSources.pokeAPI.getList(offset),
-    },
-    Pokemon: {
-      name: pokemon => capitalize(pokemon.name)
-    },
-    PokemonSpriteSheetCollection: {
-      default: sprites => ({
-        back: sprites["back_default"],
-        front: sprites["front_default"]
-      }),
-      female: sprites => extractOptionalSprites(sprites, "back_female", "front_female"),
-      shiny: sprites => extractOptionalSprites(sprites, "back_shiny", "front_shiny"),
-      shinyFemale: sprites => extractOptionalSprites(sprites, "back_shinyFemale", "front_shinyFemale"),
-    }
-  };
+//   const resolvers = {
+//     Query: {
+//       pokemons: (_source, { offset }, { dataSources }) =>
+//         dataSources.pokeAPI.getList(offset),
+//     },
+//     Pokemon: {
+//       name: pokemon => capitalize(pokemon.name)
+//     },
+//     PokemonSpriteSheetCollection: {
+//       default: sprites => ({
+//         back: sprites["back_default"],
+//         front: sprites["front_default"]
+//       }),
+//       female: sprites => extractOptionalSprites(sprites, "back_female", "front_female"),
+//       shiny: sprites => extractOptionalSprites(sprites, "back_shiny", "front_shiny"),
+//       shinyFemale: sprites => extractOptionalSprites(sprites, "back_shinyFemale", "front_shinyFemale"),
+//     }
+//   };
 
-  return new ApolloServer({
-    dataSources,
-    typeDefs,
-    resolvers,
-    introspection: true,
-    playground: true,
-  });
-}
+//   return new ApolloServer({
+//     dataSources,
+//     typeDefs,
+//     resolvers,
+//     introspection: true,
+//     playground: true,
+//   });
+// }
 
-module.exports = { createServer };
+// module.exports = { createServer };
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+module.exports = app;
