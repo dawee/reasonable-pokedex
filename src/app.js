8b95501087277c409facc6f1f57208bd4c8d3e2a
Oskar M. Grande <oskar@keatext.ai>
2019-05-13 19:57:34 -0400

    Temporarily deactivate postgres and persistence boilerplate code

--- src/app.js
+++ src/app.js
@@ -132,12 +132,12 @@ app.use(cors());
 
 server.applyMiddleware({ app });
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
-    process.env.DB_HOST
-  }:${process.env.DB_PORT}/${process.env.DB_NAME}`
-);
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(
+//   `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
+//     process.env.DB_HOST
+//   }:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// );
 
 // class User extends Sequelize.Model {}
 // User.init(
