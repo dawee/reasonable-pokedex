b706d5d0573f8f49edff18f52034b2d3dac8fad1
David Corticchiato <david.corticchiato@gmail.com>
2019-05-16 07:03:51 -0400

    add connection from outside query

--- src/app.js
+++ src/app.js
@@ -88,6 +88,25 @@ const dataSources = () => {
 
 class User extends Sequelize.Model {}
 
+const { Client } = require("pg");
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT)
+});
+
+client.connect(function(err) {
+  if (err) {
+    console.log("error");
+  } else {
+    console.log("ok");
+    client.close();
+  }
+});
+
 const resolvers = {
   Query: {
     foo: () => {
